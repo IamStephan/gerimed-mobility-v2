ac5c055c0e8c31137dad8f5b9d90740d9539d90d
I_Am_Stephan <stephanburger54@gmail.com>
2020-03-17 17:37:56 +0200

    testing useState. I likey

--- src/sections/hero/hero.js
+++ src/sections/hero/hero.js
@@ -1,57 +1,146 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import Image from 'react-image';
 import { Button } from '@material-ui/core';
+import { Spring } from 'react-spring/renderprops';
 
 import { Section } from '../../components/page/page';
 
-import globals from '../../utils/globals.scss';
 import styles from './hero.module.scss';
 
-export default class Hero extends PureComponent {
-  render() {
-    return (
-      <div className={styles['hero']}>
-        <Image
-          alt='Hero'
-          src={require('../../static/production/hero_cropped_1.jpg')}
-          className={styles['img']} />
-        
-        <Section>
-          <div className={styles['hero-content']}>
-            <div className={styles['left']}>
-              <h1 className={styles['title']}>
-                Aiding you, to live a better life.
-              </h1>
+export default function Hero() {
+  const [loaded, setLoaded] = useState(false)
 
-              <p className={styles['content']}>
-                Helping you live a normal life with exceptional quality tools and equipment.
-              </p>
+  return (
+    <div className={styles['hero']}>
+      <Image
+        alt='Hero'
+        src={require('../../static/production/hero_cropped_1.jpg')}
+        className={styles['img']} 
+        onLoad={() => setLoaded(true)}
+      />
+      
+      <Section>
+        <div className={styles['hero-content']}>
+          <div className={styles['left']}>
+            <Spring
+              from={{
+                opacity: 0,
+                x: -100
+              }}
 
-              <div className={styles['actions']}>
-                <Button
-                  variant='contained'
-                  color='primary'
-                  size='large'
-                  className={styles['btn-1']}
+              to={{
+                opacity: 1,
+                x: 0
+              }}
+
+              immediate={!loaded}
+              reverse={!loaded}
+              reset={loaded}
+
+              config={{
+                delay: 200
+              }}
+            >
+              {props => (
+                <h1
+                  className={styles['title']}
+                  style={{
+                    opacity: props.opacity,
+                    transform: `translateX(${props.x}px)`
+                  }}
                 >
-                  Our Shop
-                </Button>
+                  Aiding you, to live a better life.
+                </h1>
+              )}
+            </Spring>
 
-                <Button
-                  variant='outlined'
-                  color='primary'
-                  className={styles['btn-2']}
-                  size='large'
+            <Spring
+              from={{
+                opacity: 0,
+                x: -100
+              }}
+
+              to={{
+                opacity: 1,
+                x: 0
+              }}
+
+              immediate={!loaded}
+              reverse={!loaded}
+              reset={loaded}
+
+              config={{
+                delay: 400
+              }}
+            >
+              {props => (
+                <p
+                  className={styles['content']}
+                  style={{
+                    opacity: props.opacity,
+                    transform: `translateX(${props.x}px)`
+                  }}
                 >
-                  Learn More
-                </Button>
-              </div>
-            </div>
+                  Helping you live a normal life with exceptional quality tools and equipment.
+                </p>
+              )}
+            </Spring>
+            
+            
+            <Spring
+              from={{
+                opacity: 0,
+                x: -100
+              }}
 
-            <div className={styles['right']} />
+              to={{
+                opacity: 1,
+                x: 0
+              }}
+
+              immediate={!loaded}
+              reverse={!loaded}
+              reset={loaded}
+
+              config={{
+                delay: 600
+              }}
+            >
+              {props => (
+                <div
+                  className={styles['actions']}
+                  style={{
+                    opacity: props.opacity,
+                    transform: `translateX(${props.x}px)`
+                  }}
+                >
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    size='large'
+                    className={styles['btn-1']}
+                  >
+                    Our Shop
+                  </Button>
+
+                  <Button
+                    variant='outlined'
+                    color='primary'
+                    className={styles['btn-2']}
+                    size='large'
+                  >
+                    Learn More
+                  </Button>
+                </div>
+              )}
+            </Spring>
+
           </div>
-        </Section>
-      </div>
-    );
-  }
+
+          <div className={styles['right']} />
+        </div>
+      </Section>
+    </div>
+  )
 }
+
